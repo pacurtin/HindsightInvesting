95a2689ad3fba2dfcbe6cf697ad9110bb52a6116
Paddy <pacurtin@tcd.ie>
2016-04-05 10:38:39 +0100

    Simplified the idea down for a more lean dev approach.

--- src/app/investments/investments-controller.js
+++ src/app/investments/investments-controller.js
@@ -22,11 +22,13 @@ angular
 
 
 
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February"];
+    $scope.series = ['Investing', 'Saving'];
     $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
+      //[65, 59, 80, 81, 56, 55, 40],
+      //[28, 48, 40, 19, 86, 27, 90]
+      [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40,65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40],
+
     ];
     $scope.onClick = function (points, evt) {
       console.log(points, evt);
