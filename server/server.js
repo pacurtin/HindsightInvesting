72a50e48a32b2b8e89ff46e129f7410c1c689e56
pacurtin <pacurtin@tcd.ie>
2016-10-02 19:29:57 +0100

    Version 1

--- server/server.js
+++ server/server.js
@@ -1,51 +1,20 @@
-/*var request = require('request');
-
-request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-})
-
-// Load the http module to create an http server.
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");*/
-
 var express = require('express');
 var request = require('request');
 var cors = require('cors');
 var app = express();
-//var fs = require("fs");
-//var __dirname = 'C:\\Users\\padraig.curtin\\Projects\\HindsightInvesting\\server';
-
-/*app.get('/listUsers', function (req, res) {
-  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-    console.log( data );
-    res.end( data );
-  });
-})*/
 
 app.use(cors({origin: 'http://localhost:3000'}));
 
-//app.get('/listUsers', function (req, res) {
-  //request('http://ichart.finance.yahoo.com/table.csv?s=GE&g=w', function (error, response, body) {
-app.get('/listUsers', function (req, res) {
+app.get('/getIndividualStockData', function (req, res) {
   request('http://ichart.finance.yahoo.com/table.csv?s='+req.query.stockTicker+'&g=w', function (error, response, body) {
     console.log(req.query.stockTicker)
     if (!error && response.statusCode == 200) {
-      //console.log(body) // Print the stock data page.
-      console.log(req.query.stockTicker)
+      console.log('Stock data retrieval success');
       res.end( body );
+    }
+    if (!error && response.statusCode == 404) {
+      console.log('Stock not found');
+      res.end('Stock not found');
     }
   })
 })
@@ -55,6 +24,6 @@ var server = app.listen(8000, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Stock data server listening at http://%s:%s", host, port)
 
 })
--- src/app/investments/investments-controller.js
+++ src/app/investments/investments-controller.js
@@ -5,6 +5,7 @@ angular.module('hindsightinvesting.investments').controller('InvestmentsCtrl', [
   $scope.formFields = {};     //declaring my own scope to allow 2 way binding and form work together.
   $scope.formFields.stockTicker='GOOG'; //clear inputs. date picker seems to clear itself.
   $scope.formFields.amount=500;//todo delete
+  $scope.formFields.date = new Date('2010-01-30T00:00:00.000Z');  //setting a default date avoids errors
 
   $scope.maxDate = new Date();
   $scope.dateSelectionTooOldFlag = false;
@@ -18,12 +19,9 @@ angular.module('hindsightinvesting.investments').controller('InvestmentsCtrl', [
 
   /*TODO
   1.Reconnect to yahoo finance + test working as expected.
-  3.Dropdown list of ticker choices from yahoo.
-  4.Bootstrap CSS stuff. Make it look cool + make graph open and close depending on wether any investments have been made or not.
-  5.Friday/Monday choice and UX for this decision.
-  6.Clear initial field(?)
-  7.Investigate consequence of blank data entries from Yahoo.
-  8.Host on Heroku.
+  2.Date cant be changed manually.
+  3.Bootstrap CSS stuff. Make it look cool + make graph open and close depending on if any investments have been made or not.
+  4.Host on Heroku.
    */
 
   /*User investment choice input table*/
@@ -38,33 +36,36 @@ angular.module('hindsightinvesting.investments').controller('InvestmentsCtrl', [
   $scope.currentValue = 0;
 
   $scope.addInvestment = function(){
-    $scope.totalInvested = $scope.totalInvested + $scope.formFields.amount;
-    var results = retrieveStockData($scope.formFields.stockTicker);//.then(function (results) {   //TODO reinstate the use of promise //.then uses promise chain to solve asynchronous call problem
-      extractRelevantStockInfo(results);
-
-      var investment = {
-        name:$scope.formFields.stockTicker,                 //Abbreviation used to uniquely identify publicly traded shares of a particular stock on a particular stock market.
-        date:$scope.formFields.date,                        //Date user has selected in the GUI.
-        nearestDate:javaScriptDatesArray[nearestDateIndex], //Closest date in data to user selection. JS date object.
-        amount:$scope.formFields.amount,                    //Amount in euro invested.
-        sharesPurchased:$scope.formFields.amount/datesAndValuesDictionary[javaScriptDatesArray[nearestDateIndex]],   //number of shares bought for "amount" euros on "date"
-        adjCloseArray:adjCloseArray,                        //Stock data from Yahoo. Stock's closing price on any given day of trading that has been amended to include any distributions and corporate actions.
-        datesArray:datesArray,                              //Dates corresponding to adjCloseArray entries.
-        javaScriptDatesArray:javaScriptDatesArray,          //Same as datesArray except in JavaScript Date format. Needed for finding nearestDateIndex. Might be useful for other stuff too.
-        nearestDateIndex:nearestDateIndex,                  //This stores the index of javaScriptDatesArray (and by extension of datesArray) which is closest to 'date'.
-        datesAndValuesDictionary:datesAndValuesDictionary,   //Using date as a key will return the closing price of that day.
-        dateSelectionTooOldFlag:$scope.dateSelectionTooOldFlag  //Use to inform user they have selected a date that is too far in the past for their chosen stock.
-      };
-      $scope.investments.push(investment);                  //Persist investment data
-
-      var earliestDate = findEarliestDate($scope.investments);
-      prepareGraphDataAndDrawGraph(earliestDate,$scope.investments);    //Create labels for graph. Need a date for every friday between the earliest investment and the current date. Then fill a second series corrosponding to portfolio values on each date.
-      updateCurrentValue();
+    var results = retrieveStockData($scope.formFields.stockTicker).then(function (results) {   //.then uses promise chain to solve asynchronous call problem
+      if(results == "Stock not found"){
+        $window.alert("Entry not valid because stock not found on Yahoo finance. Examples of valid choices include 'Goog', 'AAPL' and 'MSFT'.");
+      }else{
+        $scope.totalInvested = $scope.totalInvested + $scope.formFields.amount;
+        extractRelevantStockInfo(results);
+        var investment = {
+          name:$scope.formFields.stockTicker,                 //Abbreviation used to uniquely identify publicly traded shares of a particular stock on a particular stock market.
+          date:$scope.formFields.date,                        //Date user has selected in the GUI.
+          nearestDate:javaScriptDatesArray[nearestDateIndex], //Closest date in data to user selection. JS date object.
+          amount:$scope.formFields.amount,                    //Amount in euro invested.
+          sharesPurchased:$scope.formFields.amount/datesAndValuesDictionary[nearestMonday(javaScriptDatesArray[nearestDateIndex])],   //number of shares bought for "amount" euros on "date"
+          adjCloseArray:adjCloseArray,                        //Stock data from Yahoo. Stock's closing price on any given day of trading that has been amended to include any distributions and corporate actions.
+          datesArray:datesArray,                              //Dates corresponding to adjCloseArray entries.
+          javaScriptDatesArray:javaScriptDatesArray,          //Same as datesArray except in JavaScript Date format. Needed for finding nearestDateIndex. Might be useful for other stuff too.
+          nearestDateIndex:nearestDateIndex,                  //This stores the index of javaScriptDatesArray (and by extension of datesArray) which is closest to 'date'.
+          datesAndValuesDictionary:datesAndValuesDictionary,  //Using date as a key will return the closing price of that day.
+          dateSelectionTooOldFlag:$scope.dateSelectionTooOldFlag,  //Use to inform user they have selected a date that is too far in the past for their chosen stock.
+          tickerDoesntExistFlag:false                         //Use to inform user they have selected a ticker that doesn't exist.
+        };
+        $scope.investments.push(investment);                  //Persist investment data
+        var earliestDate = findEarliestDate($scope.investments);
+        prepareGraphDataAndDrawGraph(earliestDate,$scope.investments);    //Create labels for graph. Need a date for every monday between the earliest investment and the current date. Then fill a second series corrosponding to portfolio values on each date.
+        updateCurrentValue();
+      }
       //$scope.formFields.stockTicker='';                   //clear inputs. date picker seems to clear itself.
       //$scope.formFields.amount='';
       $scope.formFields.stockTicker='GOOG'; //clear inputs. date picker seems to clear itself.
       $scope.formFields.amount=500;
-   // });
+    });
   };
 
   $scope.removeInvestment = function(index){
@@ -81,10 +82,16 @@ angular.module('hindsightinvesting.investments').controller('InvestmentsCtrl', [
 
   function retrieveStockData(ticker) {
     //e.g. ticker = 'GOOG'
-    //return InvestmentsService.getStockData(ticker).then(function (results) {
-    //  return JSON.parse($scope.UTIL.csv2Json(results.data.slice(0, -1)));  //need to slice off a return at end of data.
-    //});
-    return JSON.parse(JSON.stringify($scope.testJson));
+    console.log("Made it here 1");
+    return InvestmentsService.getStockData(ticker).then(function (results) {
+      if(results.data == "Stock not found"){
+        return "Stock not found";
+      }else{
+        return JSON.parse($scope.UTIL.csv2Json(results.data.slice(0, -1)));  //need to slice off a return at end of data.
+      }
+
+    });
+    //return JSON.parse(JSON.stringify($scope.testJson));
   }
 
 
@@ -97,7 +104,8 @@ angular.module('hindsightinvesting.investments').controller('InvestmentsCtrl', [
       if (stockJsonArray.hasOwnProperty(key)) {
         adjCloseArray.push(stockJsonArray[key]['Adj Close']);
         datesArray.push(stockJsonArray[key].Date);
-        datesAndValuesDictionary[convertStringDateToJsDate(stockJsonArray[key].Date)]=stockJsonArray[key]['Adj Close'];
+        datesAndValuesDictionary[nearestMonday(convertStringDateToJsDate(stockJsonArray[key].Date))]=stockJsonArray[key]['Adj Close'];
+        //datesAndValuesDictionary[convertStringDateToJsDate(stockJsonArray[key].Date)]=stockJsonArray[key]['Adj Close'];
       }
     }
 
@@ -170,8 +178,8 @@ angular.module('hindsightinvesting.investments').controller('InvestmentsCtrl', [
   }
 
   function prepareGraphDataAndDrawGraph(earliestDate,investments){
-    //1.set labels array. start at earliest date. add label for every friday(monday?) between earliest and today. Set data values array here too
-    var dateLabel=earliestDate;
+    //1.set labels array. start at earliest date. add label for every monday between earliest and today. Set data values array here too
+    var dateLabel=nearestMonday(earliestDate); //date label must be a monday
     var dateLabels=[];  //dates for x axis
     var dateValues=[];  //portfolio value on this date, y axis.
     var value=0;
@@ -198,6 +206,13 @@ angular.module('hindsightinvesting.investments').controller('InvestmentsCtrl', [
     $scope.data.push(dateValues);  //chartJS data is in the form of [[series1],[series2]]. Effectively a multi dimensional array. Each array is a line series.
     $scope.labels=stripDownLabels(dateLabels);
 
+  }
+
+  function nearestMonday(date) {
+    var day = date.getDay() || 7;
+    if( day !== 1 )
+        date.setHours(-24 * (day - 1));
+    return new Date(date.setHours(0, 0, 0, 0));
   }
 
   function addDays(date, days) {
